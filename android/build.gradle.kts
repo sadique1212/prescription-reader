0ba76c45f9e07fb3018da6c6a03ae004eb131a77
sadique1212 <sadique_m@ar.iitr.ac.in>
2026-05-31 20:18:40 +0530

    changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,11 @@
-
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
     configurations.all {
         resolutionStrategy {
             force("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
         }
     }
+}
